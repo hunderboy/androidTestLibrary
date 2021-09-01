3cbd2f82f1aee08783b17d7523dbb03f6653d332
hunderboy <masury2011@gmail.com>
2021-09-01 15:49:01 +0900

    first commit

---
+++ Testlib/src/main/java/com/example/testlib/ToastClass.kt
@@ -0,0 +1,13 @@
+package com.example.testlib
+
+import android.content.Context
+import android.widget.Toast
+
+class ToastClass {
+
+    companion object {
+        fun showToast(context: Context, message: String) = Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+
+}
